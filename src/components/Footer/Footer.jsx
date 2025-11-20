a0365647aab065cc7c701eee6561f074e64e85e1
Ali Nadirov <ali.nadirov.designer@gmail.com>
2025-11-21 04:57:42 +0900

    Footer SVG bottom spacing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="footer-brand">Ali Front-End</div>
         <div className="footer-copy">© 2025 Ali Front-End. All rights reserved.</div>
       </div>
-      <svg viewBox="0 0 1200 120" preserveAspectRatio="xMidYMid meet">
+      <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="wave">
         <path
           d="M0,20 C150,80 350,10 600,30 C850,50 1050,80 1200,20 L1200,120 L0,120 Z"
           fill="#fdc435"
